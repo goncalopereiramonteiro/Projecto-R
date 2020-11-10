57fbfb3fcd05b07670f9200527b7dfeb1c65074e
goncalo-a21807061 <a21807061@alunos.ulht.pt>
2020-11-10 22:52:06 +0000

    versao pré-final

--- projeto.R
+++ projeto.R
@@ -1,13 +1,18 @@
 setwd("C:/Users/g1a9p/Documents/R/Projecto-R")
 
-
+library(ggplot2)
 library(readxl)
+install.packages("factoextra")
+library("factoextra")
+
+
+#Leitura do Ficheiro e passagem de variáveis para formato contínuo
 CreditCard <- read_excel("CreditCard.xlsx", col_types = c("text", "numeric", "numeric","numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric","numeric", "numeric"))
 
 summary(CreditCard)
 View(CreditCard)
 
-
+#Transformar Colunas em variáveis
 BalanceFrequency <- CreditCard$BALANCE_FREQUENCY
 Purchases <- CreditCard$PURCHASES
 InstallmentsPurchases <- CreditCard$INSTALLMENTS_PURCHASES
@@ -17,26 +22,21 @@ CashAdvanceFrequency <- CreditCard$CASH_ADVANCE_FREQUENCY
 CashAdvanceTrx <- CreditCard$CASH_ADVANCE_TRX
 PurchasesTrx <- CreditCard$PURCHASES_TRX
 CreditLimit <- CreditCard$CREDIT_LIMIT
-View(CreditLimit)
-summary(InstallmentsPurchases)
 
-#Remover outlier CashAdvanceFrequency
-#CashAdvanceFrequency.outliers = subset(CashAdvanceFrequency, Mileage>1000)
-#CashAdvanceFrequency.no.outliers = subset(CashAdvanceFrequency, Mileage<=1000)
 
-#Remover outliers Purchases
-#Purchases.outliers = subset(Purchases, Mileage>3000)
-#Purchases.no.outliers = subset(Purchases, Mileage<=3000)
 
-#Remover outliers InstallmentsPurchases
-#InstallmentsPurchases.outliers = subset(Purchases, Mileage>15000)
-#InstallmentsPurchases.outliers = subset(Purchases, Mileage<=15000)
+#Remover outliers CreditCard
+CreditCard.no.outliers = subset(CreditCard,CreditCard$CASH_ADVANCE_FREQUENCY<1.0 & CreditCard$CREDIT_LIMIT<24000 & CreditCard$PURCHASES<30000 & CreditCard$INSTALLMENTS_PURCHASES<14000 & CreditCard$CASH_ADVANCE_TRX<56 & CreditCard$PURCHASES_TRX < 240)
+boxplot(CashAdvanceFrequency.no.outliers)
+summary(CashAdvanceFrequency.no.outliers)
+View(CreditCard.no.outliers)
+
 
 
 #CreditLimit -> Introduzir os NA's com a m?dia
-for(i in 1:length(CreditLimit)) {
-  if(is.na(CreditLimit[i])) {
-    CreditLimit[i] = 4494
+for(i in 1:length(CreditCard.no.outliers$CREDIT_LIMIT)) {
+  if(is.na(CreditCard.no.outliers$CREDIT_LIMIT[i])) {
+    CreditCard.no.outliers$CREDIT_LIMIT[i] = 4494
   }
 }
 summary(CreditLimit)
@@ -44,12 +44,40 @@ summary(CreditLimit)
 
 
 #PurchasesInstallmentsFrequency -> Introduzir NAs com a media
-for(i in 1:length(PurchasesInstallmentsFrequency)) {
-  if(is.na(PurchasesInstallmentsFrequency[i])) {
-    PurchasesInstallmentsFrequency[i] = 0.3505
+for(i in 1:length(CreditCard.no.outliers$PURCHASES_INSTALLMENTS_FREQUENCY)) {
+  if(is.na(CreditCard.no.outliers$PURCHASES_INSTALLMENTS_FREQUENCY[i])) {
+    CreditCard.no.outliers$PURCHASES_INSTALLMENTS_FREQUENCY[i] = 0.3505
   }
 }
-summary(PurchasesInstallmentsFrequency)
 
 
-boxplot(InstallmentsPurchases)
+
+#Criação de um novo DataSet
+CreditCard_final <- cbind(CreditCard.no.outliers$PURCHASES_FREQUENCY,CreditCard.no.outliers$PURCHASES_TRX)
+View(CreditCard_final)
+
+
+#Scale
+m = apply(CreditCard_final,2,mean) #medias das colunas/variaveis
+s = apply(CreditCard_final, 2, sd) #desvio padrao das variaveis
+CreditCard_scale = scale(CreditCard_final, m ,s)
+
+
+
+
+#Gerar o algoritmo K-Means
+CreditCardkm = kmeans(CreditCard_scale,3)
+CreditCardkm
+
+
+#Visualizar
+fviz_cluster(CreditCardkm , geom ="point", data = as.data.frame(CreditCard_scale)) 
+
+
+#testar K-Means
+CreditCardkm = kmeans(CreditCard_scale,2)
+CreditCardkm = kmeans(CreditCard_scale,4)
+CreditCardkm = kmeans(CreditCard_scale,5)
+CreditCardkm = kmeans(CreditCard_scale,6)
+fviz_cluster(CreditCardkm , geom ="point", data = CreditCard_scale) 
+
